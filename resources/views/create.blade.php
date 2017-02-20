9475befb54e90c37d6604672b3c8b683884a4777
Trevor Strong <trevor.strong@go.edgehill.ac.uk>
2017-02-20 22:58:48 +0000

    Added the html5 structure into the create.blade.php file along with adding ARIA rags to help add more accessbility into the project. For example, certain assistive technologies such as screen readers will know were the main sections are like the header and footer because the sections are supported with a banner tag as well as a contentinfo for the footer.

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -5,6 +5,16 @@
     <title>Create Poll</title>
   </head>
   <body>
+    <header="banner">
     <h1>Create Poll</h1>
+    <nav role="navigation">
+    </nav>
+  </header>
+  <main role="main">
+
+  </main>
+  <footer="contentinfo">
+  <small>© Copyright 2017, Polls2Go</small>
+  </footer>
   </body>
 </html>
