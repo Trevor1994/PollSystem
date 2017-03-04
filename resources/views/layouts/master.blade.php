6e973ae1778d1ca2697e2f24f75184805adc2cf5
Trevor Strong <trevor.strong@go.edgehill.ac.uk>
2017-03-04 22:05:41 +0000

    Conncted the application to the database by editing the DB_DATABASE, DB_USERNAME and the DB_PASSWORD entries.

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>PollSystem</title>
+  </head>
+
+  <body>
+    <header="banner">
+      @include (layouts.nav);
+    </header>
+  <main role="main">
+
+  </main>
+  <footer="contentinfo">
+  </footer>
+  </body>
+</html>
