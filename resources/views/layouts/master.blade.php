7ce109c77c0b231c44606dfb2a03e04556b349f5
Trevor Strong <trevor.strong@go.edgehill.ac.uk>
2017-04-07 01:23:09 +0100

    Added the footer which has the copyright logo along with the application name,

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -40,4 +40,25 @@
             <p class="lead">Cover is a one-page template for building simple and beautiful home pages. Download, edit the
               text, and add your own fullscreen background photo to make it your own.</p>
           </div>
+
+          <div class="mastfoot">
+           <div class="inner">
+             <small>&copy; Copyright 2017, Polls2Go</small>
+           </div>
+         </div>
+       </div>
+     </div>
+   </div>
+
+   <!-- Bootstrap core JavaScript
+   ================================================== -->
+   <!-- Placed at the end of the document so the pages load faster -->
+   <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
+   <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
+   <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+   <script src="../../dist/js/bootstrap.min.js"></script>
+   <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+   <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
+ </body>
+</html>
   </body>
