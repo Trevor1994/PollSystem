d1a94ca40d7b9f4a9bae1a1c9e52079944762624
Trevor Strong <trevor.strong@go.edgehill.ac.uk>
2017-04-07 01:33:07 +0100

    Added the styling for the navigation links along with the body.

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -13,7 +13,7 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
 
     <!-- Custom styles for this template -->
-    <link href="cover.css" rel="stylesheet">
+    <link href="home.css" rel="stylesheet">
   </head>
 
   <body>
