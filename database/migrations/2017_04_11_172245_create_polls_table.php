7f68e31b5e7db0229d4b2e1827d37c7069ca1316
Trevor Strong <trevor.strong@go.edgehill.ac.uk>
2017-04-11 18:35:48 +0100

    migrated the create_polls_table so it can now be seen on mysql workbench.

--- database/migrations/2017_04_11_172245_create_polls_table.php
+++ database/migrations/2017_04_11_172245_create_polls_table.php
@@ -15,6 +15,8 @@ class CreatePollsTable extends Migration
     {
         Schema::create('polls', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('title');
+            $table->text('body');
             $table->timestamps();
         });
     }
