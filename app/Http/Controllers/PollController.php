f6f6aa0938fe7b6d29dbd2a5345715b166fafc0c
Trevor Strong <trevor.strong@go.edgehill.ac.uk>
2017-04-13 15:17:39 +0100

    Added a new store function in the PostController.php file as this will create a new post using the request data as well as saving it to the database and re-directing the user to the home page.

--- app/Http/Controllers/PollController.php
+++ app/Http/Controllers/PollController.php
@@ -11,19 +11,31 @@ class PollController extends Controller
     public function index()
 
     {
-       return view('pages/create');
+       return view('pages.index');
     }
+
 
     public function show()
 
     {
-      return view('pages/create');
+      return view('pages.show');
     }
+
 
     public function create()
 
     {
-      return view('pages/create');
+      return view('pages.create');
+    }
+
+    public function store()
+
+    {
+        // Create a new post using the request data
+
+        // Save it to the database
+
+        // And then redirect to the home page
     }
 
 }
